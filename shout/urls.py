ef54d9854ab35772bb7b521be3a6cd8cfad0f29b
Nour <noor.syron@gmail.com>
2013-11-22 03:30:49 +0100

    test 1

--- shout/urls.py
+++ shout/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'shout.views.home', name='home'),
+    url(r'^$', 'shout.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
