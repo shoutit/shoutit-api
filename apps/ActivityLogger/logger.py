d97d2424075b8f257aea0e9a73df18521d22e008
Nour <noor.syron@gmail.com>
2013-11-28 15:18:37 +0100

    added apps folder

---
+++ apps/ActivityLogger/logger.py
@@ -0,0 +1,15 @@
+from apps.ActivityLogger.models import Activity
+
+class Logger(object):
+	@staticmethod
+	def log(request, activity=None, type=0, data={}):
+		if request is None:
+			return
+		if not hasattr(request, 'request_object'):
+			return
+		if activity is None:
+			activity = Activity(type=type, request=request.request_object)
+		else:
+			activity.request = request.request_object
+		activity.save()
+		activity.add_data(data)
