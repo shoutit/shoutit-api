45ef625b03613c4d3119e08d2bdb93b6dd71e8cc
Philip Martzok <philip.martzok@gmail.com>
2014-08-21 13:19:29 +0200

    - added moar api tests

--- apps/shoutit/tests/test_api.py
+++ apps/shoutit/tests/test_api.py
@@ -2,7 +2,8 @@ from django.test import TestCase
 from django.test.client import Client
 from django.db import connection
 from apps.shoutit.models import PredefinedCity
-
+import json
+import string
 
 def patchDatabase():
     with open("deploy_scripts/deploy.sql", "r") as myfile:
@@ -22,8 +23,8 @@ class APITestCase(TestCase):
     def setUp(self):
         patchDatabase()
         createCities()
-        self.client = Client()
-        response = self.client.post('/api/signup/', {
+        self.testClient = Client()
+        response = self.testClient.post('/api/signup/', {
             'firstname': 'philip',
             'lastname': 'dizzl',
             'email': 'philip@bitstars.com',
@@ -33,7 +34,42 @@ class APITestCase(TestCase):
         self.assertEqual(response.status_code, 201)
         self.assertEqual(response['Content-Type'], 'application/json; charset=utf-8')
 
-    def test_get_shouts(self):
-        response = self.client.get('/api/shouts/stream/')
+    def test_api(self):
+        self.shout_stream()
+        self.nearby_shouts()
+        self.shout_clusters()
+
+    def shout_stream(self):
+        response = self.testClient.get('/api/shouts/stream/')
+        print type(response)
+        print response.content
+        print type(response.content)
+        print string.find(response.content, "[")
+        parsed = json.loads(response.content)
+        print type(parsed)
+        print(parsed[0])
+        parsed2 = json.loads(parsed[0])
+        print json.dumps(parsed2, sort_keys=True, indent=4)
+        self.assertEqual(response.status_code, 200)
+
+    def nearby_shouts(self):
+        response = self.client.get('/api/shouts/nearby', {
+            # Aachen BB "((50.75315090356295, 6.008417663574164), (50.79537870169773, 6.171753463745063))"
+            'DownLeftLat': '50.75315090356295',
+            'DownLeftLng': '6.008417663574164',
+            'UpRightLat': '50.79537870169773',
+            'UpRightLng': '6.171753463745063'
+        }, follow=True)
+        print(response)
+        self.assertEqual(response.status_code, 200)
+
+    def shout_clusters(self):
+        response = self.client.get('/api/shouts/nearby/clusters/', {
+            # Aachen BB "((50.75315090356295, 6.008417663574164), (50.79537870169773, 6.171753463745063))"
+            'bottomRightLatitude': '50.75315090356295',
+            'topLeftLongitude': '6.008417663574164',
+            'topLeftLatitude': '50.79537870169773',
+            'bottomRightLongitude': '6.171753463745063'
+        }, follow=True)
         print(response)
         self.assertEqual(response.status_code, 200)
