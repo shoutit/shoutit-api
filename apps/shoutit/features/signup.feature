3e3d016f5462d561d781e79e7b7f90436787cd37
muaazo <mmodabashi@gmail.com>
2015-01-12 23:21:53 +0100

    Signup feature v0

--- apps/shoutit/features/signup.feature
+++
@@ -1,9 +0,0 @@
-# Created by muaazo at 12.12.14
-Feature: Signup
-  grant access to shoutit
-
-  Scenario: User opens the application for the first time
-    Given user installed the application
-    And Application is running
-    When user authenticated
-    Then access token is returned
---
+++ apps/shoutit/features/steps/sign_up.py
@@ -0,0 +1,162 @@
+from behave import *
+
+use_step_matcher("re")
+
+@given("user has no shoutit account")
+def step_impl(context):
+    """
+    :type context behave.runner.Context
+    """
+    pass
+
+
+@step("user has a valid email address")
+def step_impl(context):
+    """
+    :type context behave.runner.Context
+    """
+    pass
+
+
+@step("user wants to sign up with his email address")
+def step_impl(context):
+    """
+    :type context behave.runner.Context
+    """
+    pass
+
+
+@when("user enters a user name")
+def step_impl(context):
+    """
+    :type context behave.runner.Context
+    """
+    pass
+
+
+@step("user enters a first name")
+def step_impl(context):
+    """
+    :type context behave.runner.Context
+    """
+    pass
+
+
+@step("user enters a last name")
+def step_impl(context):
+    """
+    :type context behave.runner.Context
+    """
+    pass
+
+
+@step("user enters a valid email address")
+def step_impl(context):
+    """
+    :type context behave.runner.Context
+    """
+    pass
+
+
+@step("user repeats email address")
+def step_impl(context):
+    """
+    :type context behave.runner.Context
+    """
+    pass
+
+
+@step("user enters a valid password")
+def step_impl(context):
+    """
+    :type context behave.runner.Context
+    """
+    pass
+
+
+@step("user repeats a matching password")
+def step_impl(context):
+    """
+    :type context behave.runner.Context
+    """
+    pass
+
+
+@then("new account is created for the user")
+def step_impl(context):
+    """
+    :type context behave.runner.Context
+    """
+    pass
+
+
+@step("user is granted access")
+def step_impl(context):
+    """
+    :type context behave.runner.Context
+    """
+    pass
+
+
+@given("user has a google account")
+def step_impl(context):
+    """
+    :type context behave.runner.Context
+    """
+    pass
+
+
+@step("user agrees to let shoutit use it's basic info")
+def step_impl(context):
+    """
+    :type context behave.runner.Context
+    """
+    pass
+
+
+@step("user lets shoutit use his/her G\+ for sign in")
+def step_impl(context):
+    """
+    :type context behave.runner.Context
+    """
+    pass
+
+
+@step("user agrees to let shoutit upload videos to his YouTube account")
+def step_impl(context):
+    """
+    :type context behave.runner.Context
+    """
+    pass
+
+
+@given("user has a Facebook account")
+def step_impl(context):
+    """
+    :type context behave.runner.Context
+    """
+    pass
+
+
+@step("user agrees that shoutit view his/her email address")
+def step_impl(context):
+    """
+    :type context behave.runner.Context
+    """
+    pass
+
+
+@step("user agrees that shoutit view his/her birthday")
+def step_impl(context):
+    """
+    :type context behave.runner.Context
+    """
+    pass
+
+
+@step("user allows shoutit to publish on his behalf")
+def step_impl(context):
+    """
+    :type context behave.runner.Context
+    """
+    pass
