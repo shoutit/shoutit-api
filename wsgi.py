6cc085b551885416c682eb4a7e3a2f03fc015074
Mo Chawich <noor.syron@gmail.com>
2015-01-30 08:57:55 +0100

    - detecting gunicorn on server [test]

--- wsgi.py
+++ wsgi.py
@@ -9,6 +9,9 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
+from django.core.handlers.wsgi import WSGIHandler
+application = Sentry(WSGIHandler())
 
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+# from django.core.wsgi import get_wsgi_application
+# application = get_wsgi_application()
