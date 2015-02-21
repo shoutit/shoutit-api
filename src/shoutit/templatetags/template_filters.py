1a9ffed5b7f42a03e395573232c4cd277adb598c
Mo Chawich <noor.syron@gmail.com>
2015-02-21 12:34:49 +0100

    - auth docs login shortcut

--- src/shoutit/templatetags/template_filters.py
+++ src/shoutit/templatetags/template_filters.py
@@ -6,7 +6,9 @@ import re
 from django import template
 from django.template.base import VariableDoesNotExist
 from django.utils.safestring import mark_safe
+from django.utils.html import escape
 from django.utils.translation import ugettext as _
+from django.core.urlresolvers import reverse, NoReverseMatch
 from widget_tweaks.templatetags.widget_tweaks import _process_field_attributes
 from django.conf import settings
 
@@ -392,3 +394,32 @@ def name2url(name):
 @register.simple_tag
 def shout_link(post):
     return _shout_link(post)
+
+
+@register.simple_tag
+def auth_login(request):
+    """
+    Include a login snippet if REST framework's login view is in the URLconf.
+    """
+    try:
+        login_url = reverse('rest_framework:login')
+    except NoReverseMatch:
+        return ''
+
+    snippet = "<a id='_user' href='{href}?next={next}'>Log in</a>".format(href=login_url, next=escape(request.path))
+    return snippet
+
+
+@register.simple_tag
+def auth_logout(request, user):
+    """
+    Include a logout snippet if REST framework's logout view is in the URLconf.
+    """
+    try:
+        logout_url = reverse('rest_framework:logout')
+    except NoReverseMatch:
+        return '<a id="_user">{user}</a>'.format(user=user)
+
+    snippet = '<a id="_user" href="{href}?next={next}" title="Log out">{user}</a>'
+
+    return snippet.format(user=user, href=logout_url, next=escape(request.path))
