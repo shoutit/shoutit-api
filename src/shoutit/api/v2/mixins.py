aacc1da7f861d74f36097751785a73fc15f6e1b3
Mo Chawich <noor.syron@gmail.com>
2015-02-27 18:41:44 +0100

    - listening 'results' field name renamed to 'users' or 'tags' based on return

--- src/shoutit/api/v2/mixins.py
+++ src/shoutit/api/v2/mixins.py
@@ -3,17 +3,22 @@
 
 """
 from __future__ import unicode_literals
+from rest_framework.pagination import PaginationSerializer
 
 
 class CustomPaginationSerializerMixin(object):
 
-    def get_custom_pagination_serializer(self, page, serializer_class):
+    def get_custom_pagination_serializer(self, page, custom_serializer_class, custom_results_field='results'):
         """
         Return a serializer instance to use with paginated data using the `serializer_class` param
         """
-        class SerializerClass(self.pagination_serializer_class):
+
+        class PaginationSerializerClass(PaginationSerializer):
+            results_field = custom_results_field
+
+        class SerializerClass(PaginationSerializerClass):
             class Meta:
-                object_serializer_class = serializer_class
+                object_serializer_class = custom_serializer_class
 
         pagination_serializer_class = SerializerClass
         context = self.get_serializer_context()
