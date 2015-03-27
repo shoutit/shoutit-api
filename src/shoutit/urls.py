3228bf06eddc08ae1c5f4215906c6e8afd27e43a
Mo Chawich <noor.syron@gmail.com>
2015-03-27 22:03:02 +0100

    - django-rq admin on /rq

--- src/shoutit/urls.py
+++ src/shoutit/urls.py
@@ -25,6 +25,9 @@ urlpatterns = patterns('',
                        # api v2
                        url(r'^v2/', include('shoutit.api.v2.urls', namespace='v2')),
 
+                       # django-rq
+                       (r'^rq/', include('django_rq.urls')),
+
                        # todo: move to web
                        url(r'^favicon\.ico$', RedirectView.as_view(url='static/img/icon.png')),
                        url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
@@ -168,4 +171,5 @@ urlpatterns = patterns('',
 # serving static files while developing locally using gunicorn
 if settings.GUNICORN and settings.LOCAL:
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
     urlpatterns += staticfiles_urlpatterns()
