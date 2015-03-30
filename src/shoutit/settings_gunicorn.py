a0bec3b50eed9e6e21b56493dadd0f7f1e4edc41
Mo Chawich <noor.syron@gmail.com>
2015-03-30 21:29:45 +0200

    - better gunicorn config

--- src/shoutit/settings_gunicorn.py
+++ src/shoutit/settings_gunicorn.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import multiprocessing
 
 # include src dir in sys.path a.k.a PYTHONPATH to be able to use env_settings.
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
@@ -9,10 +10,10 @@ from shoutit.settings_env import *
 if LOCAL:
     bind = '0.0.0.0:8000'
     workers = 1
-    timeout = 5 * 60
 else:
     bind = '0.0.0.0:8001'
-    workers = 1
+    workers = multiprocessing.cpu_count() * 2 + 1
+    max_requests = 1000
 
 errorlog = accesslog = '-'
 loglevel = 'info'
