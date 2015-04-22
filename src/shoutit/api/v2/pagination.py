f42a032e19b74dc6b745ef4de6b469b2ae50a1a4
Mo Chawich <mo.chawich@gmail.com>
2015-04-23 00:00:57 +0200

    - bugs fixes and backward compatibility

--- src/shoutit/api/v2/pagination.py
+++ src/shoutit/api/v2/pagination.py
@@ -12,6 +12,7 @@ from rest_framework.response import Response
 from rest_framework.templatetags.rest_framework import replace_query_param
 from rest_framework.pagination import CursorPagination
 from datetime import datetime
+from rest_framework.utils.urls import remove_query_param
 from shoutit.api.api_utils import get_current_uri
 
 
@@ -238,6 +239,7 @@ class PageNumberIndexPagination(PageNumberPagination):
     page_size_query_param = 'page_size'
     max_page_size = 50
     results_field = 'results'
+    template = 'rest_framework/pagination/previous_and_next.html'
 
     def paginate_queryset(self, index_queryset, request, view=None):
         """
@@ -309,3 +311,27 @@ class PageNumberIndexPagination(PageNumberPagination):
                 pass
 
         return self.page_size
+
+    def get_html_context(self):
+        return {
+            'previous_url': self.get_previous_link(),
+            'next_url': self.get_next_link()
+        }
+
+    def get_next_link(self):
+        return ""
+        # if not self.page.has_next():
+        #     return None
+        # url = self.request.build_absolute_uri()
+        # page_number = self.page.next_page_number()
+        # return replace_query_param(url, self.page_query_param, page_number)
+
+    def get_previous_link(self):
+        return ""
+        # if not self.page.has_previous():
+        #     return None
+        # url = self.request.build_absolute_uri()
+        # page_number = self.page.previous_page_number()
+        # if page_number == 1:
+        #     return remove_query_param(url, self.page_query_param)
+        # return replace_query_param(url, self.page_query_param, page_number)
