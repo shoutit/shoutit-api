abc094ffb68b4a43d9d23f404f6ab512e71a2309
Mo Chawich <mo.chawich@gmail.com>
2015-04-26 01:35:00 +0200

    - useradmin update

--- src/shoutit/admin.py
+++ src/shoutit/admin.py
@@ -55,8 +55,17 @@ class CustomUserChangeForm(UserChangeForm):
 
 class CustomUserAdmin(UserAdmin):
     save_on_top = True
-    list_display = ('id', 'username', 'email', 'first_name', 'last_name', 'is_staff', 'last_login')
+    list_display = ('id', 'username', 'email', 'first_name', 'last_name', 'is_staff', 'is_superuser'
+                    , 'is_active', 'is_activated', 'last_login')
     list_per_page = 50
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_activated', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    list_filter = ('is_active', 'is_activated', 'is_staff', 'is_superuser', 'groups')
 
     form = CustomUserChangeForm
 
