ffea5d9f6e4afef72bef6345dd505c6fc4fe0340
Mo Chawich <mo.chawich@gmail.com>
2015-04-29 21:02:50 +0200

    - cooler shouts paging.

--- src/shoutit/api/v2/pagination.py
+++ src/shoutit/api/v2/pagination.py
@@ -302,7 +302,7 @@ class PageNumberIndexPagination(PageNumberPagination):
             self.num_results = index_response.hits.total if self.page else 0
         self.num_pages = int(math.ceil(self.num_results / (page_size * 1.0)))
         if getattr(self, 'max_page_number_exceeded', False):
-            self.page_number = min(self.num_pages + 1, max_page_number)
+            self.page_number = min(self.num_pages + 1, max_page_number + 1)
         else:
             self.page_number = page_number
         self.page_size = page_size
