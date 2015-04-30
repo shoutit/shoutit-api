bb3fa930998a9b62fb73359834e49e77a6886278
Mo Chawich <mo.chawich@gmail.com>
2015-04-30 22:48:15 +0200

    - index shouts command update

--- src/shoutit/management/commands/index_shouts.py
+++ src/shoutit/management/commands/index_shouts.py
@@ -12,8 +12,13 @@ from shoutit.models import Shout
 class Command(BaseCommand):
     help = 'Index all shouts'
 
+    def add_arguments(self, parser):
+        # Positional arguments
+        parser.add_argument('from', nargs=1, type=int)
+        parser.add_argument('to', nargs=1, type=int)
+
     def handle(self, *args, **options):
         # todo find better way to index all shouts no matter how many there is
-        for shout in Shout.objects.filter(is_disabled=False, muted=False)[:1000]:
+        for shout in Shout.objects.filter(is_disabled=False, muted=False)[options.get('from')[0]:options.get('to')[0]]:
             save_shout_index(None, shout)
         self.stdout.write('Successfully indexed all shouts.')
