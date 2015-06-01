cb431ac1ad7cebf8d5449a416cc0229b91d93ab1
Mo Chawich <mo.chawich@gmail.com>
2015-06-01 14:47:58 +0200

    - added image file upload to TagAdmin

--- src/shoutit/admin.py
+++ src/shoutit/admin.py
@@ -1,4 +1,7 @@
 from __future__ import unicode_literals
+import uuid
+import boto
+from django.conf import settings
 from django.contrib import admin
 from django.contrib.admin import DateFieldListFilter
 from django.contrib.auth.admin import UserAdmin
@@ -78,7 +81,8 @@ class UserEmailFilter(admin.SimpleListFilter):
                 return queryset.filter(email__icontains='@sale.craigslist.org')
         elif queryset.model == Profile:
             if self.value() == 'shoutit':
-                return queryset.filter(~Q(user__email=''), ~Q(user__email__icontains='@sale.craigslist.org'))
+                return queryset.filter(~Q(user__email=''),
+                                       ~Q(user__email__icontains='@sale.craigslist.org'))
             if self.value() == 'yes':
                 return queryset.filter(~Q(user__email=''))
             if self.value() == 'no':
@@ -100,8 +104,9 @@ class CustomUserChangeForm(UserChangeForm):
 class CustomUserAdmin(UserAdmin):
     save_on_top = True
     list_display = (
-        'id', 'username', '_profile', 'email', 'first_name', 'last_name', '_messaging'
-        , 'is_staff', 'is_test', 'is_superuser', 'is_active', 'is_activated', 'last_login', 'created_at')
+        'id', 'username', '_profile', 'email', 'first_name', 'last_name', '_messaging',
+        'is_staff', 'is_test', 'is_superuser', 'is_active', 'is_activated', 'last_login',
+        'created_at')
     list_per_page = 50
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
@@ -121,7 +126,7 @@ class CustomUserAdmin(UserAdmin):
         return [url(r'^([-\w]+)/password/$',
                     self.admin_site.admin_view(self.user_change_password),
                     name='shoutit_user_password_change')
-               ] + super(UserAdmin, self).get_urls()
+                ] + super(UserAdmin, self).get_urls()
 
     def _profile(self, obj):
         return '<a href="%s">Profile</a>' % (
@@ -149,7 +154,8 @@ class ProfileAdmin(admin.ModelAdmin):
     search_fields = ['user__first_name', 'user__last_name', 'user__username', 'user__email', 'bio']
     readonly_fields = ('video', '_user')
     exclude = ('user',)
-    list_filter = ('country', 'city', 'gender', UserEmailFilter, ('created_at', DateFieldListFilter))
+    list_filter = (
+    'country', 'city', 'gender', UserEmailFilter, ('created_at', DateFieldListFilter))
     ordering = ('-created_at',)
 
     def _user(self, obj):
@@ -239,6 +245,24 @@ class LinkedGoogleAccountAdmin(admin.ModelAdmin):
 #
 # admin.site.register(BusinessCategory, BusinessCategoryAdmin)
 
+class TagChangeForm(forms.ModelForm):
+    image_file = forms.FileField()
+
+    class Meta:
+        model = Tag
+        fields = '__all__'
+
+    def clean_image_file(self):
+        s3 = boto.connect_s3(settings.AWS_ACCESS_KEY_ID, settings.AWS_SECRET_ACCESS_KEY)
+        bucket = s3.get_bucket('shoutit-tag-image-original')
+        image_file = self.cleaned_data['image_file']
+        filename = "%s-%s.jpg" % (uuid.uuid4(), self.cleaned_data['name'])
+        key = bucket.new_key(filename)
+        key.set_metadata('Content-Type', 'image/jpg')
+        key.set_contents_from_file(image_file)
+        s3_image_url = 'https://tag-image.static.shoutit.com/%s' % filename
+        self.cleaned_data['image'] = s3_image_url
+
 
 # Tag
 @admin.register(Tag)
@@ -246,6 +270,7 @@ class TagAdmin(admin.ModelAdmin):
     list_display = ('name', 'stream')
     search_fields = ('name',)
     readonly_fields = ('creator',)
+    form = TagChangeForm
 
 
 @admin.register(Category)
@@ -295,6 +320,7 @@ class ConversationAdmin(admin.ModelAdmin):
     def _users(self, conversation):
         def user_line(u):
             return '- %s | %s' % (user_link(u), reply_link(conversation, u))
+
         return '<br/>'.join([user_line(user) for user in conversation.users.all()])
 
     _users.short_description = 'Users'
@@ -329,7 +355,8 @@ class MessageAdmin(admin.ModelAdmin):
     _user.short_description = 'User'
 
     def _conversation(self, message):
-        conversation_link = reverse('admin:shoutit_conversation_change', args=(message.conversation.pk,))
+        conversation_link = reverse('admin:shoutit_conversation_change',
+                                    args=(message.conversation.pk,))
         return '<a href="%s">%s</a>' % (conversation_link, message.conversation.pk)
 
     _conversation.allow_tags = True
@@ -397,7 +424,7 @@ class ConfirmTokenAdmin(admin.ModelAdmin):
 
 @admin.register(DBUser)
 class DBUserAdmin(admin.ModelAdmin):
-    list_display = ('id', '_user', 'db_link', 'shout','created_at')
+    list_display = ('id', '_user', 'db_link', 'shout', 'created_at')
     ordering = ('-created_at',)
 
     def _user(self, obj):
@@ -438,10 +465,12 @@ class DBCLConversationAdmin(admin.ModelAdmin):
 
     def _from_user(self, obj):
         return user_link(obj.from_user)
+
     _from_user.allow_tags = True
 
     def _to_user(self, obj):
         return user_link(obj.to_user)
+
     _to_user.allow_tags = True
 
 
