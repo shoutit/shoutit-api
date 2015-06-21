5163b8c36937bab55223227eeb137ee7815194ce
Mo Chawich <mo.chawich@gmail.com>
2015-06-21 14:04:02 +0200

    - reply upgrade

--- src/shoutit/management/commands/reply_sss.py
+++ src/shoutit/management/commands/reply_sss.py
@@ -16,15 +16,20 @@ class Command(BaseCommand):
     arabic_replies = ["العفو تم البيع", "شكرا، بس للاسف تم البيع"]
     english_replies = ["sorry sold", "it is sold already sorry", "thanks for your message but it has been already sold", "sold"]
 
+    def add_arguments(self, parser):
+        # Positional arguments
+        parser.add_argument('--days', default=2, type=int)
+
     def handle(self, *args, **options):
         # get conversations
         now = timezone.now()
         today = now.replace(hour=0, minute=0, second=0, microsecond=0)
-        yesterday = today + datetime.timedelta(days=-1)
-        two_days_ago = today + datetime.timedelta(days=-2)
-        conversations = Conversation.objects.filter(created_at__gte=two_days_ago,
-                                                    created_at__lt=yesterday,
+        nine_hours_ago = today + datetime.timedelta(hours=-9)
+        days_ago = today + datetime.timedelta(days=-options['days'])
+        conversations = Conversation.objects.filter(created_at__gte=days_ago,
+                                                    created_at__lt=nine_hours_ago,
                                                     shout__is_sss=True)
+        replies_count = 0
         for conversation in conversations:
             shout = conversation.attached_object
             sss_user = shout.user
@@ -40,5 +45,5 @@ class Command(BaseCommand):
             shout.save()
             # delete dbcl conversations
             DBCLConversation.objects.filter(to_user=sss_user).delete()
-
-        self.stdout.write("Successfully replied on behalf of sss users")
+            replies_count += 1
+        self.stdout.write("Successfully replied on behalf of %s sss users" % replies_count)
