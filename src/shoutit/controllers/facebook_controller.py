d98a4c856a0e2f467e6140880e1d90b56b79ec6a
Mo Chawich <mo.chawich@gmail.com>
2015-07-27 00:07:54 +0200

    - using requests instead of urllib2

--- src/shoutit/controllers/facebook_controller.py
+++ src/shoutit/controllers/facebook_controller.py
@@ -4,13 +4,12 @@
 """
 from __future__ import unicode_literals
 import json
-import urllib
-import urllib2
 import urlparse
 
 from django.core.exceptions import ObjectDoesNotExist
 from django.conf import settings
 from django.db.backends.postgresql_psycopg2.base import IntegrityError
+import requests
 from rest_framework.exceptions import ValidationError
 from shoutit.api.v2.exceptions import (FB_LINK_ERROR_TRY_AGAIN, FB_LINK_ERROR_EMAIL,
                                        FB_LINK_ERROR_NO_LINK)
@@ -43,20 +42,22 @@ def user_from_facebook_auth_response(auth_response, initial_user=None):
     return user
 
 
+# todo: check!
 def exchange_code(request, code):
     # Get Access Token using the Code then make an authResponse
     try:
         qs = request.META['QUERY_STRING'].split('&code')[0]
-        redirect_uri = urllib.quote('%s%s?%s' % (settings.SITE_LINK, request.path[1:], qs))
-        exchange_url = 'https://graph.facebook.com/oauth/access_token?client_id=%s&redirect_uri=%s&client_secret=%s&code=%s' % (
-            settings.FACEBOOK_APP_ID,
-            redirect_uri,
-            settings.FACEBOOK_APP_SECRET,
-            code
-        )
-
-        response = urllib2.urlopen(exchange_url, timeout=20)
-        params = dict(urlparse.parse_qsl(response.read()))
+        # redirect_uri = urllib.quote('%s%s?%s' % (settings.SITE_LINK, request.path[1:], qs))
+        redirect_uri = settings.SITE_LINK + request.path[1:] + qs
+        exchange_url = 'https://graph.facebook.com/oauth/access_toke'
+        params = {
+            'client_id': settings.FACEBOOK_APP_ID,
+            'client_secret': settings.FACEBOOK_APP_SECRET,
+            'redirect_uri': redirect_uri,
+            'code': code
+        }
+        response = requests.get(exchange_url, params=params, timeout=20)
+        params = dict(urlparse.parse_qsl(response.content))
     except Exception, e:
         print e.message
         return None
@@ -69,19 +70,26 @@ def exchange_code(request, code):
 
 
 def extend_token(short_lived_token):
-    query = "client_id={}&client_secret={}&grant_type=fb_exchange_token&fb_exchange_token={}"
-    query = query.format(settings.FACEBOOK_APP_ID, settings.FACEBOOK_APP_SECRET, short_lived_token)
-    exchange_url = 'https://graph.facebook.com/oauth/access_token?{}'.format(query)
+    exchange_url = 'https://graph.facebook.com/oauth/access_token'
+    params = {
+        'client_id': settings.FACEBOOK_APP_ID,
+        'client_secret': settings.FACEBOOK_APP_SECRET,
+        'grant_type': "fb_exchange_token",
+        'fb_exchange_token': short_lived_token
+    }
     try:
-        response = urllib2.urlopen(exchange_url, timeout=20)
-        params = dict(urlparse.parse_qsl(response.read()))
-        if not ('access_token' in params and 'expires' in params):
-            raise Exception('access_token or expires not in params: {}'.format(json.dumps(params)))
-    except Exception, e:
+        response = requests.get(exchange_url, params=params, timeout=20)
+        if response.status_code != 200:
+            raise ValueError("Invalid access token: %s" % response.content)
+        response_params = dict(urlparse.parse_qsl(response.content))
+        access_token = response_params.get('access_token')
+        expires = response_params.get('expires')
+        if not any((access_token, expires)):
+            raise ValueError('access_token or expires not in response: %s' % response.content)
+    except (requests.RequestException, ValueError) as e:
         debug_logger.error("Facebook token extend error: %s" % str(e))
         raise FB_LINK_ERROR_TRY_AGAIN
-
-    return {'access_token': params.get('access_token'), 'expires': params.get('expires')}
+    return response_params
 
 
 def link_facebook_account(user, facebook_access_token):
@@ -138,11 +146,11 @@ def unlink_facebook_user(user, strict=True):
 
 
 def fb_user_from_facebook_access_token(facebook_access_token):
-    graph_url = 'https://graph.facebook.com/me?access_token={}'.format(facebook_access_token)
+    graph_url = 'https://graph.facebook.com/me'
+    params = {'access_token': facebook_access_token}
     try:
-        response = urllib2.urlopen(graph_url, timeout=20)
-        fb_user = json.loads(response.read())
+        fb_user = requests.get(graph_url, params=params, timeout=20).json()
         return fb_user
-    except (urllib2.HTTPError, ValueError) as e:
+    except (requests.RequestException, ValueError) as e:
         debug_logger.error("Facebook Graph error: %s" % str(e))
         raise FB_LINK_ERROR_TRY_AGAIN
