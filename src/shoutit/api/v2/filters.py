f246714a6312c408c491b7b64b7ee184186d2287
Mo Chawich <mo.chawich@gmail.com>
2015-11-21 15:55:57 +0100

    Showing shouts from all countries at home [added todo]

--- src/shoutit/api/v2/filters.py
+++ src/shoutit/api/v2/filters.py
@@ -185,6 +185,11 @@ class HomeFilterBackend(filters.BaseFilterBackend):
         user = view.get_object()
         data = request.query_params
         listening = []
+
+        # Todo: figure way to show shouts that are
+        # - from users / pages listened to by the users [any country]
+        # - from tags listened to by the user [only his country]
+
         # country = user.location.get('country')
         # if country:
         #     index_queryset = index_queryset.filter('term', country=country)
