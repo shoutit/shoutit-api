f32501199eb0e1be00840fb4fabbdb8601821cdc
Mo Chawich <mo.chawich@gmail.com>
2016-02-26 19:32:11 +0100

    Add ability to index shouts and locations and flush the ES indices much easier

--- src/shoutit/management/commands/flush_index.py
+++ src/shoutit/management/commands/flush_index.py
@@ -5,13 +5,30 @@
 from __future__ import unicode_literals
 from django.core.management.base import BaseCommand
 from django.conf import settings
+from elasticsearch import NotFoundError
+
 import shoutit
 
 
 class Command(BaseCommand):
     help = 'Flush the entire ES Index.'
 
+    def add_arguments(self, parser):
+        group = parser.add_mutually_exclusive_group(required=True)
+        group.add_argument('--index', nargs=1, type=str)
+        group.add_argument('--full_index', nargs=1, type=str)
+
     def handle(self, *args, **options):
-        index_name = settings.ENV
-        shoutit.ES.indices.delete(index_name)
-        self.stdout.write("Successfully flushed '{}' index. Make sure to restart the server immediately".format(index_name))
+        index = options.get('index')
+        full_index = options.get('full_index')
+        if full_index:
+            index_name = full_index[0]
+        else:
+            index_name = settings.ENV + '_' + index[0]
+
+        try:
+            shoutit.ES.indices.delete(index_name)
+            self.stdout.write("Successfully flushed '{}' index. Make sure to restart the server immediately".format(index_name))
+        except NotFoundError as e:
+            self.stderr.write("Failed to flush index '%s'" % index_name)
+            self.stderr.write("Error: " + str(e))
--- src/shoutit/management/commands/index_shouts.py
+++ src/shoutit/management/commands/index_shouts.py
@@ -5,27 +5,37 @@
 from __future__ import unicode_literals
 
 from django.core.management.base import BaseCommand
-from elasticsearch.helpers import streaming_bulk
+from elasticsearch.helpers import bulk
 from shoutit.controllers.shout_controller import shout_index_from_shout
 from shoutit.models import Shout
 from shoutit import ES
 
 
 class Command(BaseCommand):
-    help = 'Index all shouts'
+    help = 'Index shouts'
 
     def add_arguments(self, parser):
-        # Positional arguments
-        parser.add_argument('from', nargs=1, type=int)
-        parser.add_argument('to', nargs=1, type=int)
+        parser.add_argument('--limit', nargs=1, type=int, default=[1000])
 
     def handle(self, *args, **options):
-        # todo find better way to index all shouts no matter how many there is
-        _from = options.get('from')[0]
-        _to = options.get('to')[0]
-        shout_index_dicts = []
-        for shout in Shout.objects.filter(is_disabled=False, muted=False)[_from:_to]:
-            shout_index_dicts.append(shout_index_from_shout(shout).to_dict(True))
-        for ok, info in streaming_bulk(ES, shout_index_dicts, chunk_size=1000):
-            self.stdout.write("Created ShoutIndex: %s" % info['index']['_id'])
-        self.stdout.write('Successfully indexed shouts from %s to %s' % (_from, _to))
+        limit = options.get('limit')[0]
+        chunk = 500
+        total_succeed = 0
+        total_failed = 0
+        for i in range(limit / chunk + (1 if limit % chunk > 0 else 0)):
+            shout_index_dicts = []
+            shouts = Shout.objects.filter(is_disabled=False, muted=False, is_indexed=False)[:chunk if limit > chunk else limit]
+            if not shouts:
+                break
+            for shout in shouts:
+                shout_index_dicts.append(shout_index_from_shout(shout).to_dict(True))
+            succeed, errors = bulk(ES, shout_index_dicts, chunk_size=250, raise_on_error=False, raise_on_exception=False)
+            total_succeed += succeed
+            failed = len(errors)
+            total_failed += failed
+            shout_ids = map(lambda x: x['_id'], shout_index_dicts)
+            failed_ids = map(lambda x: x['index']['_id'], errors)
+            succeed_ids = set(shout_ids) - set(failed_ids)
+            Shout.objects.filter(id__in=succeed_ids).update(is_indexed=True)
+            self.stdout.write('-- Chunk %0.3d: Successfully indexed %s shouts with %s errors' % (i + 1, succeed, failed))
+        self.stdout.write('Successfully indexed %s shouts with %s errors' % (total_succeed, total_failed))
