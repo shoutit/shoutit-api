63d9f41b7c11609cf6ac2ecac123c5c940a6483b
Mo Chawich <mo.chawich@gmail.com>
2016-03-18 22:33:47 +0100

    Init v3 errors

--- src/shoutit/api/v3/exceptions.py
+++ src/shoutit/api/v3/exceptions.py
@@ -4,12 +4,52 @@
 """
 from __future__ import unicode_literals
 
-from rest_framework.exceptions import ValidationError
+from django.core import exceptions as django_exceptions
+from django.http import Http404
+from django.utils.translation import ugettext_lazy as _
+from rest_framework import exceptions as drf_exceptions, status
+from rest_framework.compat import set_rollback
+from rest_framework.response import Response
 
-FB_LINK_ERROR_TRY_AGAIN = ValidationError({'error': "Could not link Facebook account, try again later."})
-FB_LINK_ERROR_EMAIL = ValidationError({'error': "Could not access user email, make sure you allowed it."})
-FB_LINK_ERROR_NO_LINK = ValidationError({'error': "No Facebook account to unlink."})
 
-GPLUS_LINK_ERROR_TRY_AGAIN = ValidationError({'error': "Could not link G+ account, try again later."})
-GPLUS_LINK_ERROR_EMAIL = ValidationError({'error': "Could not access user email, make sure you allowed it."})
-GPLUS_LINK_ERROR_NO_LINK = ValidationError({'error': "No G+ account to unlink."})
+def exception_handler(exc, context):
+    """
+    Returns the response that should be used for any given exception.
+
+    By default we handle the REST framework `APIException`, and also
+    Django's built-in `Http404` and `PermissionDenied` exceptions.
+
+    Any unhandled exceptions may return `None`, which will cause a 500 error
+    to be raised.
+    """
+    if isinstance(exc, drf_exceptions.APIException):
+        headers = {}
+        if getattr(exc, 'auth_header', None):
+            headers['WWW-Authenticate'] = exc.auth_header
+        if getattr(exc, 'wait', None):
+            headers['Retry-After'] = '%d' % exc.wait
+
+        if isinstance(exc.detail, (list, dict)):
+            data = exc.detail
+        else:
+            data = {'detail': exc.detail}
+
+        set_rollback()
+        return Response(data, status=exc.status_code, headers=headers)
+
+    elif isinstance(exc, Http404):
+        msg = _('Not found.')
+        data = {'detail': unicode(msg)}
+
+        set_rollback()
+        return Response(data, status=status.HTTP_404_NOT_FOUND)
+
+    elif isinstance(exc, django_exceptions.PermissionDenied):
+        msg = _('Permission denied.')
+        data = {'detail': unicode(msg)}
+
+        set_rollback()
+        return Response(data, status=status.HTTP_403_FORBIDDEN)
+
+    # Note: Unhandled exceptions will raise a 500 error.
+    return None
