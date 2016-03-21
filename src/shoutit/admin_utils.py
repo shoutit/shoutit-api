84eb4ba8d86c540edaaffdecda8cd907f3a7ac95
Mo Chawich <mo.chawich@gmail.com>
2016-03-21 11:34:59 +0100

    Show items that have no name in admin

--- src/shoutit/admin_utils.py
+++ src/shoutit/admin_utils.py
@@ -54,7 +54,7 @@ def item_link(item):
     if not item:
         return 'no item'
     item_url = reverse('admin:shoutit_item_change', args=(item.pk,))
-    return '<a href="%s">%s</a>' % (item_url, item.name)
+    return '<a href="%s">%s</a>' % (item_url, unicode(item))
 
 
 def reply_link(conversation, user):
