d86ef5b2b175580a139b4c12f14ecb85540d0a7b
Mo Chawich <mo.chawich@gmail.com>
2016-03-30 23:26:53 +0200

    Add different pusher credentials based on env [fix]

--- src/shoutit_pusher/settings.py
+++ src/shoutit_pusher/settings.py
@@ -2,14 +2,19 @@ from django.conf import settings
 from pusher.requests import RequestsBackend
 
 SHOUTIT_PUSHER_SETTINGS = getattr(settings, "SHOUTIT_PUSHER_SETTINGS", {})
-SHOUTIT_PUSHER_SETTINGS.setdefault("app_id", "121664")
 if settings.PROD:
+    # shoutit-api-prod
+    SHOUTIT_PUSHER_SETTINGS.setdefault("app_id", "121664")
     SHOUTIT_PUSHER_SETTINGS.setdefault("key", "86d676926d4afda44089")
     SHOUTIT_PUSHER_SETTINGS.setdefault("secret", "e7ef2a69a659f642fe0b")
 elif settings.DEV:
+    # shoutit-api-dev
+    SHOUTIT_PUSHER_SETTINGS.setdefault("app_id", "193537")
     SHOUTIT_PUSHER_SETTINGS.setdefault("key", "afde270c82795bdc4ad0")
     SHOUTIT_PUSHER_SETTINGS.setdefault("secret", "da808243ac99a592e070")
 else:
+    # shoutit-api-local
+    SHOUTIT_PUSHER_SETTINGS.setdefault("app_id", "193590")
     SHOUTIT_PUSHER_SETTINGS.setdefault("key", "d6a98f27e49289344791")
     SHOUTIT_PUSHER_SETTINGS.setdefault("secret", "5ac1c01bc04e90c13339")
 SHOUTIT_PUSHER_SETTINGS.setdefault("ssl", True)
