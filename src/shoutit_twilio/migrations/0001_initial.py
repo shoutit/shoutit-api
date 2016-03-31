d5e326b480cb384ef10056ffafe493723eaefbdb
Mo Chawich <mo.chawich@gmail.com>
2016-04-01 00:17:44 +0200

    Add better video authentication

---
+++ src/shoutit_twilio/migrations/0001_initial.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+from django.conf import settings
+import uuid
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='VideoClient',
+            fields=[
+                ('id', models.UUIDField(default=uuid.uuid4, serialize=False, editable=False, primary_key=True)),
+                ('created_at', models.DateTimeField(auto_now_add=True, verbose_name='Creation time', null=True)),
+                ('modified_at', models.DateTimeField(auto_now=True, verbose_name='Modification time', null=True)),
+                ('token', models.CharField(max_length=1000)),
+                ('user', models.OneToOneField(related_name='video_client', to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'abstract': False,
+            },
+        ),
+    ]
--- src/shoutit_twilio/views.py
+++ src/shoutit_twilio/views.py
@@ -4,11 +4,18 @@
 """
 from __future__ import unicode_literals
 
+import uuid
+
+from django.core.exceptions import ValidationError
+from django.db import IntegrityError
 from rest_framework import viewsets
 from rest_framework.decorators import list_route
 from rest_framework.response import Response
 from twilio.access_token import AccessToken, ConversationsGrant
 
+from shoutit.api.v3.exceptions import ShoutitBadRequest, RequiredParameter, InvalidParameter
+from shoutit.models import User
+from shoutit_twilio.models import VideoClient
 from .settings import SHOUTIT_TWILIO_SETTINGS
 
 
@@ -17,7 +24,7 @@ class ShoutitTwilioViewSet(viewsets.ViewSet):
     Shoutit Twilio API Resources.
     """
 
-    @list_route(methods=['post'], suffix='Authorize')
+    @list_route(methods=['post'], suffix='Authorize Video Client')
     def video_auth(self, request):
         """
         Create a video chat endpoint.
@@ -26,31 +33,86 @@ class ShoutitTwilioViewSet(viewsets.ViewSet):
         <pre><code>
         {
           "token": "eyJhbGciOiAiSFMyNTYiLCAidHlwIjogIkpXVCIsICJjdHkiOiAidHdpbGlvLWZwYTt2PTEifQ.eyJpc3MiOiAiU0s3MDFlYzE",
-          "identity": "17729791284"
+          "identity": "7c6ca4737db3447f936037374473e61f"
         }
         </code></pre>
         ---
         """
+        user = request.user
+        if hasattr(user, 'video_client'):
+            video_client = user.video_client
+        else:
+            # Get credentials
+            account_sid = SHOUTIT_TWILIO_SETTINGS['TWILIO_ACCOUNT_SID']
+            api_key = SHOUTIT_TWILIO_SETTINGS['TWILIO_API_KEY']
+            api_secret = SHOUTIT_TWILIO_SETTINGS['TWILIO_API_SECRET']
 
-        # Get credentials
-        account_sid = SHOUTIT_TWILIO_SETTINGS['TWILIO_ACCOUNT_SID']
-        api_key = SHOUTIT_TWILIO_SETTINGS['TWILIO_API_KEY']
-        api_secret = SHOUTIT_TWILIO_SETTINGS['TWILIO_API_SECRET']
+            # Create an Access Token
+            token = AccessToken(account_sid, api_key, api_secret)
 
-        # Create an Access Token
-        token = AccessToken(account_sid, api_key, api_secret)
+            # Set the Identity of this token using random uuid.hex (not to have the hyphens)
+            identity = uuid.uuid4().hex
+            token.identity = identity
 
-        # Set the Identity of this token
-        token.identity = request.user.username
+            # Grant access to Conversations
+            grant = ConversationsGrant()
+            grant.configuration_profile_sid = SHOUTIT_TWILIO_SETTINGS['TWILIO_CONFIGURATION_SID']
+            token.add_grant(grant)
 
-        # Grant access to Conversations
-        grant = ConversationsGrant()
-        grant.configuration_profile_sid = SHOUTIT_TWILIO_SETTINGS['TWILIO_CONFIGURATION_SID']
-        token.add_grant(grant)
+            # Create VideoClient
+            jwt_token = token.to_jwt()
+            try:
+                video_client = VideoClient.create(id=identity, user=user, token=jwt_token)
+            except (ValidationError, IntegrityError) as e:
+                msg = "Couldn't authorize you to make video calls"
+                raise ShoutitBadRequest(message=msg, developer_message=unicode(e))
 
         # Return token info
         res = {
-            'identity': token.identity,
-            'token': token.to_jwt()
+            'identity': video_client.identity,
+            'token': video_client.token
+        }
+        return Response(res)
+
+    @list_route(methods=['get'], suffix='Retrieve Identity')
+    def video_identity(self, request):
+        """
+        Retrieve video client identity to make a call
+        <pre><code>
+        GET: /twilio/video_identity?profile=username
+        </code></pre>
+
+        ###REQUIRES AUTH
+
+        ###Response
+        <pre><code>
+        {
+          "identity": "7c6ca4737db3447f936037374473e61f"
+        }
+        </code></pre>
+
+        Returns 403 if the call is now allowed.
+        ---
+        """
+        user = request.user
+
+        other_username = request.query_params.get('profile')
+        if not other_username:
+            raise RequiredParameter('profile')
+
+        try:
+            other_user = User.objects.get(username=other_username)
+        except User.DoesNotExist:
+            msg = "Profile with username %s doesn't exist" % other_username
+            raise InvalidParameter('profile', message=msg)
+
+        if hasattr(other_user, 'video_client'):
+            video_client = other_user.video_client
+        else:
+            msg = "Profile with username %s is not online" % other_username
+            raise InvalidParameter('profile', message=msg)
+
+        res = {
+            'identity': video_client.identity
         }
         return Response(res)
