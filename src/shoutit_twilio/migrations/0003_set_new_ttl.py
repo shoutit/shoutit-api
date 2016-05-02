447170a39080f01623142c68b3421fb585323115
Mo Chawich <mo.chawich@gmail.com>
2016-05-03 01:13:43 +0200

    Set new ttl for older video clients

---
+++ src/shoutit_twilio/migrations/0003_set_new_ttl.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+from ..models import VideoClient
+from ..settings import SHOUTIT_TWILIO_SETTINGS
+
+
+def set_new_ttl(apps, schema_editor):
+    VideoClient.objects.update(ttl=SHOUTIT_TWILIO_SETTINGS['TOKEN_TTL'])
+
+
+def undo_reset_ttl(apps, schema_editor):
+    VideoClient.objects.update(ttl=3600)
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('shoutit_twilio', '0002_auto_20160502_2328'),
+    ]
+
+    operations = [
+        migrations.RunPython(set_new_ttl, undo_reset_ttl)
+    ]
