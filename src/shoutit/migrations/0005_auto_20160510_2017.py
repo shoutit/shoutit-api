e424d10e62e6562c22a66953adf73871042fa1e7
Mo Chawich <mo.chawich@gmail.com>
2016-05-10 20:18:42 +0200

    Add migrations for new message attachment `profile`

---
+++ src/shoutit/migrations/0005_auto_20160510_2017.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('shoutit', '0004_auto_20160510_0834'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='messageattachment',
+            name='type',
+            field=models.SmallIntegerField(choices=[(0, 'shout'), (1, 'location'), (2, 'media'), (3, 'profile')]),
+        ),
+    ]
