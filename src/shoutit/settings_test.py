ff1b6436008028293d8eed5b63c5a2f86b773b8a
st4lk <myhappydo@gmail.com>
2016-05-21 18:04:36 +0300

    implement discover tests [refs #170]

---
+++ src/shoutit/settings_test.py
@@ -0,0 +1,6 @@
+from shoutit.settings import *
+
+DDF_FIELD_FIXTURES = {
+    'django.contrib.postgres.fields.hstore.HStoreField': lambda: {},
+}
+DDF_FILL_NULLABLE_FIELDS = False
--- tests/base.py
+++ tests/base.py
@@ -9,6 +9,15 @@ class BaseTestCase(APITestCase):
     def reverse(cls, url_name, *args, **kwargs):
         return reverse(cls.url_namespace + ':' + url_name, *args, **kwargs)
 
+    def assertIdsEqual(self, dict_iter, objects_iter, order=False):
+        id_list_1 = [str(o['id']) for o in dict_iter]
+        id_list_2 = [str(o.id) for o in objects_iter]
+        self.assertEqual(len(id_list_1), len(id_list_2))
+        if order:
+            self.assertEqual(id_list_1, id_list_2)
+        else:
+            self.assertEqual(set(id_list_1), set(id_list_2))
+
     def assert200(self, response):
         self.assertEqual(response.status_code, 200)
 
