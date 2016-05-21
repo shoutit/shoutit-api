819aa4bc6d3cbbc5f1fb16e37970d9604886c1b4
st4lk <myhappydo@gmail.com>
2016-05-22 00:35:34 +0300

    add tests for pusher [refs #175]

--- src/shoutit/settings_test.py
+++ src/shoutit/settings_test.py
@@ -1,5 +1,7 @@
 from shoutit.settings import *
 
+REST_FRAMEWORK['TEST_REQUEST_DEFAULT_FORMAT'] = 'json'
+
 DDF_FIELD_FIXTURES = {
     'django.contrib.postgres.fields.hstore.HStoreField': lambda: {},
 }
