bae024c237148826999731aacfdb149fc2ff28e7
st4lk <myhappydo@gmail.com>
2016-05-29 21:47:35 +0300

    increase test coverage for notification [refs #173]

--- tests/v3/test_notifications.py
+++ tests/v3/test_notifications.py
@@ -9,7 +9,9 @@ from shoutit.models import Notification
 from shoutit.models.auth import APNSDevice
 from common.constants import (
     NOTIFICATION_TYPE_MESSAGE, NOTIFICATION_TYPE_LISTEN,
-    NOTIFICATION_TYPE_BROADCAST, NOTIFICATION_TYPE_STATS_UPDATE)
+    NOTIFICATION_TYPE_BROADCAST, NOTIFICATION_TYPE_STATS_UPDATE,
+    NOTIFICATION_TYPE_INCOMING_VIDEO_CALL,
+    NOTIFICATION_TYPE_MISSED_VIDEO_CALL)
 from tests.base import BaseTestCase, mocked_pusher
 
 
@@ -25,22 +27,19 @@ class NotificationsTestCase(BaseTestCase):
 
         cls.device = G(APNSDevice, user=cls.user)
 
-        cls.n1 = N(Notification, to_user=cls.user, type=NOTIFICATION_TYPE_MESSAGE)
-        cls.n1.attached_object = cls.another_user
-        cls.n1.save()
+        cls.n1 = cls._create_notif(NOTIFICATION_TYPE_MESSAGE)
 
-        cls.n2 = N(Notification, to_user=cls.user, type=NOTIFICATION_TYPE_LISTEN)
-        cls.n2.attached_object = cls.another_user
-        cls.n2.save()
+        cls.n2 = cls._create_notif(NOTIFICATION_TYPE_LISTEN)
+        cls.n3 = cls._create_notif(NOTIFICATION_TYPE_INCOMING_VIDEO_CALL,
+                                   attached_object=cls.user.profile)
+        cls.n4 = cls._create_notif(NOTIFICATION_TYPE_MISSED_VIDEO_CALL)
+        cls.n5 = cls._create_notif(NOTIFICATION_TYPE_BROADCAST,
+                                   attached_object=False)
 
-        cls.n3 = G(Notification, to_user=cls.user,
-                   type=NOTIFICATION_TYPE_BROADCAST)
+        cls.n6 = cls._create_notif(NOTIFICATION_TYPE_LISTEN,
+                                   to_user=cls.another_user)
 
-        cls.n4 = G(Notification, to_user=cls.another_user,
-                   type=NOTIFICATION_TYPE_LISTEN)
-
-        cls.notifications = [cls.n1, cls.n2, cls.n3, cls.n4]
-        cls.user_notifications = [cls.n2, cls.n3]
+        cls.user_notifications = [cls.n2, cls.n3, cls.n4, cls.n5]
 
     # list
 
@@ -59,7 +58,7 @@ class NotificationsTestCase(BaseTestCase):
 
     def test_notification_reset_marked_as_read(self):
         self.client.login(username=self.user.username, password='123')
-        self._change_read(self.notifications, False)
+        Notification.objects.all().update(is_read=False)
         resp = self.client.post(self.reverse(self.url_name_reset))
         self.assert202(resp)
         self.assert_ids_equal(
@@ -99,7 +98,8 @@ class NotificationsTestCase(BaseTestCase):
         self.assert400(resp)
 
     def test_notification_read_changed(self):
-        self._change_read([self.n2], False)
+        self.n2.save()
+        Notification.objects.filter(pk=self.n2.pk).update(is_read=False)
         self.client.login(username=self.user.username, password='123')
         resp = self.client.post(
             self.reverse(self.url_name_read, kwargs={'id': self.n2.pk}))
@@ -108,7 +108,7 @@ class NotificationsTestCase(BaseTestCase):
 
     @patch.object(mocked_pusher, 'trigger')
     def test_notification_read_event_sent(self, m_trigger):
-        self._change_read(self.user_notifications, False)
+        Notification.objects.all().update(is_read=False)
         self.client.login(username=self.user.username, password='123')
         m_trigger.reset_mock()
         self.client.post(
@@ -116,20 +116,25 @@ class NotificationsTestCase(BaseTestCase):
         self.assert_pusher_event(
             m_trigger, str(NOTIFICATION_TYPE_STATS_UPDATE),
             attached_object_partial_dict={
-                'unread_notifications_count': 1,
+                'unread_notifications_count': 3,
                 'unread_conversations_count': 0,
             })
 
     @patch.object(apns, 'apns_send_bulk_message')
     def test_notification_ios_badge_set(self, m_apns_bulk):
-        self._change_read(self.user_notifications, False)
+        Notification.objects.all().update(is_read=False)
         self.client.login(username=self.user.username, password='123')
         self.client.post(
             self.reverse(self.url_name_read, kwargs={'id': self.n2.pk}))
         self.assert_ios_badge_set(m_apns_bulk, [self.device.registration_id],
-                                  badge=1)
+                                  badge=3)
 
-    def _change_read(self, notifications, is_read):
-        for n in notifications:
-            n.is_read = is_read
+    @classmethod
+    def _create_notif(cls, n_type, to_user=None, attached_object=None):
+        to_user = to_user or cls.user
+        n = N(Notification, to_user=to_user, type=n_type)
+        attached_object = attached_object or cls.another_user
+        if attached_object:
+            n.attached_object = attached_object
             n.save()
+        return n
