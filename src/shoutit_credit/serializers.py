e6d0033fa018d0aae933b5472121602d13fce7fe
Mo Chawich <mo.chawich@gmail.com>
2016-06-17 16:22:33 +0200

    Disabkle PromoteOptionSerializer validation [tmp fix]

--- src/shoutit_credit/serializers.py
+++ src/shoutit_credit/serializers.py
@@ -8,8 +8,6 @@ from rest_framework import serializers
 from shoutit_credit.models.profile import InvitationCode
 from .models import PromoteShouts, CreditTransaction, PromoteLabel, ShoutPromotion
 
-from shoutit.api.v3.serializers.base import AttachedUUIDObjectMixin  # Import it from base not to create import loop in `serialize_attached_object`
-
 
 class CreditTransactionSerializer(serializers.ModelSerializer):
     created_at = serializers.IntegerField(source='created_at_unix')
@@ -26,22 +24,13 @@ class PromoteLabelSerializer(serializers.ModelSerializer):
         fields = ('name', 'description', 'color', 'bg_color')
 
 
-class PromoteOptionSerializer(serializers.ModelSerializer, AttachedUUIDObjectMixin):
+class PromoteOptionSerializer(serializers.ModelSerializer):
     label = PromoteLabelSerializer(read_only=True)
 
     class Meta:
         model = PromoteShouts
         fields = ('id', 'name', 'description', 'label', 'credits', 'days')
         extra_kwargs = {'name': {'read_only': True}, 'description': {'read_only': True}}
-
-    def to_internal_value(self, data):
-        # Validate when passed as attached object
-        ret = self.to_internal_attached_value(data, force_validation=True)
-        if ret:
-            return ret
-
-        validated_data = super(PromoteOptionSerializer, self).to_internal_value(data)
-        return validated_data
 
 
 class PromoteShoutSerializer(serializers.Serializer):
@@ -60,7 +49,7 @@ class PromoteShoutSerializer(serializers.Serializer):
         }
 
 
-class ShoutPromotionSerializer(serializers.ModelSerializer, AttachedUUIDObjectMixin):
+class ShoutPromotionSerializer(serializers.ModelSerializer):
     label = PromoteLabelSerializer(read_only=True)
 
     class Meta:
