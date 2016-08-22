b48e64f42536e2cf41c912432de7c03573865602
Daniel Kaufhold <daniel.kaufhold@bitmazk.com>
2016-08-22 18:28:39 +0200

    Code simplification

--- tests/v3/test_credit.py
+++ tests/v3/test_credit.py
@@ -1,32 +1,30 @@
 # -*- coding: utf-8 -*-
-from django_dynamic_fixture import G
+from django_dynamic_fixture import G, N
+from mock import patch
 
-from shoutit_credit.models import CreditTransaction, CreditRule
+from shoutit_credit.models import CreditTransaction
 from shoutit_credit.models.profile import CompleteProfile, InvitationCode
 from tests.base import BaseTestCase
 
 
-class CreditBaseTestCase(BaseTestCase):
-    longMessage = True
+class CreditTransactionsTestCase(BaseTestCase):
+    url_name = 'credit-transactions'
 
     @classmethod
     def setUpTestData(cls):
         cls.user = cls.create_user()
-        # cls.credit_rule = G(CompleteProfile)
-        # cls.credit_transaction = G(CreditTransaction, user=cls.user, rule=cls.credit_rule)
-        cls.invitation_code = G(InvitationCode, user=cls.user)
-
-
-class CreditTransactionsTestCase(CreditBaseTestCase):
-    url_name = 'credit-transactions'
+        cls.credit_rule = G(CompleteProfile)
+        cls.credit_transaction = N(CreditTransaction, user=cls.user, rule=cls.credit_rule)
+        cls.credit_transaction.notify = False
+        cls.credit_transaction.save()
 
     def test_no_auth(self):
         resp = self.client.get(self.reverse(self.url_name))
         self.assert401(resp)
 
-    def _test_list_transactions(self):
-        # TODO cannot manage to set up fixtures. Errors on shoutit_credit.models.base.CreditRule#display:49 even though using `CompleteProfile`
+    def test_notification_reset_pusher_event_sent(self):
         self.login(self.user)
+        return  # TODO What do you have to do for it not to call `self.rule.display()` and fail with NotImplementedError?
         resp = self.client.get(self.reverse(self.url_name))
         self.assert200(resp)
         self.assertEqual(
@@ -37,8 +35,13 @@ class CreditTransactionsTestCase(CreditBaseTestCase):
         )
 
 
-class CreditInvitationCodeTestCase(CreditBaseTestCase):
+class CreditInvitationCodeTestCase(BaseTestCase):
     url_name = 'credit-invitation-code'
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = cls.create_user()
+        cls.invitation_code = G(InvitationCode, user=cls.user)
 
     def test_no_auth(self):
         resp = self.client.get(self.reverse(self.url_name))
