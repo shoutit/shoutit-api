451f7a71c8a7258dd62c918b1b9fdb44047560d8
Mo Chawich <mo.chawich@gmail.com>
2017-01-18 23:51:57 +0100

    Connect securely to ES on AWS

--- src/shoutit/__init__.py
+++ src/shoutit/__init__.py
@@ -1,5 +1,8 @@
-from settings import info, ES_URL
+from elasticsearch import Elasticsearch, RequestsHttpConnection
 from elasticsearch_dsl.connections import connections
+from requests_aws4auth import AWS4Auth
+
+from django.conf import settings
 
 """
 =================================
@@ -8,7 +11,8 @@ from elasticsearch_dsl.connections import connections
 """
 # some monkey patching for global imports
 import monkey_patches  # NOQA
-info('Monkeys: Loaded')
+
+settings.info('Monkeys: Loaded')
 
 """
 =================================
@@ -16,9 +20,20 @@ info('Monkeys: Loaded')
 =================================
 """
 # Define a default global Elasticsearch client
-ES = connections.create_connection(hosts=[ES_URL])
-
+if 'es.amazonaws.com' in settings.ES_URL:
+    # Connect using IAM  based authentication on AWS
+    awsauth = AWS4Auth(settings.AWS_ACCESS_KEY_ID, settings.AWS_SECRET_ACCESS_KEY, 'eu-west-1', 'es')
+    ES = Elasticsearch(
+        hosts=[settings.ES_URL],
+        http_auth=awsauth,
+        use_ssl=True,
+        verify_certs=True,
+        connection_class=RequestsHttpConnection
+    )
+    connections.add_connection(alias='default', conn=ES)
+else:
+    ES = connections.create_connection(hosts=[settings.ES_URL])
 
 default_app_config = 'shoutit.appconfig.ShoutitConfig'
 
-info("==================================================")
+settings.info("==================================================")
